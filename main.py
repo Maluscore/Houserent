a46f2aed790ec73367a04e2cfaf87482687c8854
TG <ghpeng1992@126.com>
2016-08-31 21:05:51 +0800

    数据成功传递给了后端，但是前端不响应响应的操作

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 from flask import render_template
+from utils import log
 
 app = Flask(__name__)
 
@@ -11,7 +14,14 @@ def index_view():
 
 @app.route('/gongyu', methods=['POST'])
 def data_post():
-    pass
+    form = request.get_json()
+    min_val = form.get('minVal', '')
+    max_val = form.get('maxVal', '')
+    log(min_val)
+    data_list = []
+    data = dict(address='天安门')
+    data_list.append(data)
+    return jsonify(data_list)
 
 
 if __name__ == '__main__':
